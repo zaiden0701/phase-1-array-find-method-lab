78bef7aeedf04a6ec5019c6a8a2baaac6c8ac99a
Aiden Zafar <+>
2022-09-27 18:00:41 -0400

    commit tree e025790e3a8747bf321f7ae23980f7c95c793e87

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function superbowlWin(record){
+    const found = record.find(isWin)
+    console.log(found)
+   // return found.year
+   //if (found.result === 'W'){
+//     return found.year
+//    }
+//     return 
+    if (found === undefined){
+        return undefined
+    } else {
+        return found.year
+    }
+}
+
+function isWin(record) {
+    return record.result === 'W'
+  }
+  
